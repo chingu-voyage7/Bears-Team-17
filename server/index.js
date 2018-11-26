feb23b85f56b4a1ed48a4d572325e816237b51e0
nikrb <nik_rb@yahoo.com>
2018-11-26 18:45:40 +0000

    create basic server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+if (process.env !== 'production') {
+  require('dotenv').config();
+};
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/test', (req, res) => {
+  res.json({ success: true, message: "OK" });
+});
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use( '/', express.static('client/build'));
+
+  app.get('*', function (req, res) {
+    res.sendFile( 'client/build/index.html');
+  });
+} else {
+  app.use(express.static('../client/build')); // FIXME: remove path.resolve(__dirname, '..', 'frontend', 'build')));
+  // Always return the main index.html, so react-router render the route in the client
+  app.get('*', (req, res) => {
+    res.sendFile('../client/build/index.html'); // path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+  });
+}
+
+app.listen( PORT, () => {
+  console.log(`Find the server at port [${PORT}]`); // eslint-disable-line no-console
+});
