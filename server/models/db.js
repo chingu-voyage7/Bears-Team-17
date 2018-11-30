6a9f0fe1164cbe09648a4bd29db3362472a215e6
psitronic <spintronic@tuta.io>
2018-11-30 09:53:38 +0100

    Move connection info to .env file

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cars',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
