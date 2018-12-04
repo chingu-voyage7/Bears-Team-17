2c18133542726ef62d511e210117fede7041f909
psitronic <spintronic@tuta.io>
2018-12-04 09:24:25 +0100

    Change comments type to Array in answerSchema

--- server/models/answers.js
+++ server/models/answers.js
@@ -6,7 +6,7 @@ const answerSchema = new Schema({
     author: {type: Schema.Types.ObjectId, ref: 'User'},
     info:  {type: String},
     text: {type: String, required: true},
-    comments: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    comments: {type: Array},
     date: {type: Date, 'default': Date.now},
     scores: {type: Number, 'default': 0},
     edited: {type: Boolean, 'default': false}
