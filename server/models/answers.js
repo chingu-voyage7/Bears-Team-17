460af52218bbb8e7c23b5f3a408b8cde318adf58
U N R E A L <38888039+unrealkaii@users.noreply.github.com>
2018-12-06 15:04:38 +0000

    Revert "Bug: reference to db models"

--- server/models/answers.js
+++ server/models/answers.js
@@ -6,7 +6,7 @@ const answerSchema = new Schema({
     author: {type: Schema.Types.ObjectId, ref: 'User'},
     info:  {type: String},
     text: {type: String, required: true},
-    comments: {type: Array},
+    comments: [{type: Schema.Types.ObjectId, ref: 'User'}],
     date: {type: Date, 'default': Date.now},
     scores: {type: Number, 'default': 0},
     edited: {type: Boolean, 'default': false}
