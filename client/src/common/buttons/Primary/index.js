afa4a30915dba2aff6c6e7b375ffafc2a74f341f
nikrb <nik_rb@yahoo.com>
2018-12-15 19:27:42 +0000

    feat: create PrimaryButton

---
+++ client/src/common/buttons/Primary/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+  color: #fff;
+  background-color: #6c6;
+  border-radius: 5px;
+  border-color: #5b5;
+  margin: 0;
+  padding: 6px 12px;
+`;
+
+export default PrimaryButton;
+
