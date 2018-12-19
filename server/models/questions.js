669768031117c458fa514cfa47be83482580a569
psitronic <spintronic@tuta.io>
2018-12-19 12:32:39 +0100

    Fix indentation problem

--- server/models/questions.js
+++ server/models/questions.js
@@ -4,16 +4,16 @@ const { Schema } = mongoose;
 
 // question schema
 const questionSchema = new Schema({
-    title:  {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'User'},
-    info:  {type: String},
-    text: {type: String, required: true},
-    tags: {type: Array},
-    answers: [{type: Schema.Types.ObjectId, ref: 'Answer'}],
-    date: {type: Date, 'default': Date.now},
-    scores: {type: Number, 'default': 0},
-    views: {type: Number, 'default': 0},
-    edited: {type: Boolean, 'default': false},
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  info: { type: String },
+  text: { type: String, required: true },
+  tags: { type: Array },
+  answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }],
+  date: { type: Date, default: Date.now },
+  scores: { type: Number, default: 0 },
+  views: { type: Number, default: 0 },
+  edited: { type: Boolean, default: false },
 });
 
 // build Question model
