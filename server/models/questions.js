01f166c972d121105f638346d8e8ad5895e3b994
psitronic <spintronic@tuta.io>
2018-12-21 21:06:24 +0100

    Add statics method questions

--- server/models/questions.js
+++ server/models/questions.js
@@ -9,6 +9,7 @@ const questionSchema = new Schema({
   text: { type: String, required: true },
   tags: { type: Array },
   answers: [{ type: Schema.Types.ObjectId, ref: 'Answers' }],
+  n_answers: { type: Number, default: 0 },
   date: { type: Date, default: Date.now },
   scores: { type: Number, default: 0 },
   views: { type: Number, default: 0 },
@@ -17,4 +18,21 @@ const questionSchema = new Schema({
 
 // build Question model
 const Questions = mongoose.model('Question', questionSchema);
+
+// Find messages by own mongoose query
+// eslint-disable-next-line func-names
+questionSchema.statics.questions = function (sortby, callback) {
+  Questions.find({})
+    .select('title author text tags answers date scores views edited')
+    .sort(sortby)
+    .populate('author', 'name')
+    .exec((err, docs) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, docs);
+      }
+    });
+};
+
 module.exports = Questions;
