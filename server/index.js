abf3ccc88d1d61b621b3a1551916bda676e76fcd
nikrb <nik_rb@yahoo.com>
2018-12-28 00:25:40 +0000

    refactor: db init

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 3001;
 const app = require('./app');
 const db = require('./models/db');
 
+db.init();
+
 app.listen(port, () => {
-  console.log('listening on port:', port);
+  console.log('app server listening on port:', port);
 });
 
