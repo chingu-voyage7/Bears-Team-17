fb9f410af1e3979f0ddbb396c806193390d9879d
nikrb <nik_rb@yahoo.com>
2018-12-28 23:57:06 +0000

    add cross button

---
+++ client/src/common/buttons/Cross/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+ 
+const cross_symbol = String.fromCharCode( 10799);
+
+const Cross = styled.button`
+  color: red;
+  font-weight: bold;
+  background-color: white;
+  border-radius: 100%;
+  border-color: #555;
+  margin: 0;
+  padding: 0.3em 0.3em;
+  line-height: 0.9em;
+`;
+
+const CrossButton = ({ onClick }) => (
+  <Cross onClick={onClick} >
+    { cross_symbol }
+  </Cross>
+);
+
+export default CrossButton;
+
