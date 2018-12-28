751dfd1b35980dd60fee9d3ca16563318a9769c4
nikrb <nik_rb@yahoo.com>
2018-12-28 23:12:51 +0000

    test: add another user and rework

--- server/test/userData.js
+++ server/test/userData.js
@@ -1,8 +1,16 @@
-const u1 = {
+/*
+userData
+first (zeroth) user is seeded to db before testing (for all tests to use)
+second user is not seeded and used to test creation
+ */
+
+module.exports = [{
   name: 'nik',
   email: 'n@e.com',
   password: 'interesting',
-};
-
-module.exports = [u1];
-
+}, {
+  name: 'francis',
+  email: 'f@e.com',
+  password: 'francispassword',
+},
+];
