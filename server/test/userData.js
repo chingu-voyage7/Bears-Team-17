d0f81eebe84d313cc82c51289ca7c4c18d9f1036
nikrb <nik_rb@yahoo.com>
2018-12-29 23:49:08 +0000

    remove redundant comment

--- server/test/userData.js
+++ server/test/userData.js
@@ -1,9 +1,3 @@
-/*
-userData
-first (zeroth) user is seeded to db before testing (for all tests to use)
-second user is not seeded and used to test creation
- */
-
 module.exports = [{
   name: 'nik',
   email: 'n@e.com',
