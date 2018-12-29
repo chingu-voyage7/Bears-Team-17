52eec43503bd8c4832a800470e9a8d3fd8fed1f8
nikrb <nik_rb@yahoo.com>
2018-12-29 00:04:40 +0000

    test: add cross button test

--- client/src/MyComponent.test.js
+++ client/src/MyComponent.test.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { render } from 'react-testing-library';
 import MyComponent from './MyComponent';
 
+const cross_symbol = String.fromCharCode( 10799);
+
 describe('MyComponent', () => {
   it('renders MyComponent', () => {
     const { container, getByText } = render(<MyComponent />);
     expect(container.firstChild).toMatchSnapshot();
     const text = getByText('Click Me');
     expect(text).toBeDefined();
+    const cross = getByText(cross_symbol);
+    expect(cross).toBeDefined();
   });
 });
 
